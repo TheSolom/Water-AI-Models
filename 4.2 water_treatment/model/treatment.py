8bdb95027016efd023989bb41796959f00fb5aea
TheSolom <eslam.01212@gmail.com>
2023-09-21 18:08:29 +0300

    pkl

---
+++ 4.2 water_treatment/model/treatment.py
@@ -0,0 +1,12 @@
+# import pickle
+# import sys
+
+# model = pickle.load(open("public/js/treatment/model.pkl", 'rb'))
+
+# a = float(sys.argv[0])
+# b = float(sys.argv[1])
+# c = float(sys.argv[2])
+
+# predictions = model.predict([[a, b, c]])
+
+print('predictions[0]')
